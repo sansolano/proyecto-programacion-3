4ff3864b48f861f65ddb55e81e16b50c3039feed
gabosolarcia <gabosolarcia@gmail.com>
2024-04-02 22:10:49 -0600

    add about us page

---
+++ dev/about/about.js
@@ -0,0 +1,8 @@
+function misionClick(){
+    document.getElementById('misionId').style.display = "block";
+    document.getElementById('visionId').style.display = "none";
+}
+function visionClick(){
+    document.getElementById('misionId').style.display = "none";
+    document.getElementById('visionId').style.display = "block";
+}
