8ec827416df7de668ecce06fbe249574e9b47484
Santiago Solano <sansol2506@gmail.com>
2024-04-06 21:42:21 -0600

    Commit Header

--- dev/components/header/header.js
+++ dev/components/header/header.js
@@ -6,8 +6,8 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <style>
-            header{
-                background-color: rgb(116, 193, 238);
+            header {
+                background-color: #ff5000;
                 display: flex;
                 align-items: center;
                 height: 100px;
@@ -15,42 +15,69 @@ class Header extends HTMLElement {
                 position: fixed;
                 left: 0px;
                 top: 0px;
+                color: white; /* Color de texto blanco */
             }
             
-            header * {
-                display: inline;
-            }
-            
-            header img{
+            header img {
                 width: 10%;
             }
             
-            nav li{
+            nav li {
                 right: 0;
                 margin: 20px;
             }
             
-            nav a{
+            nav a {
                 text-transform: uppercase;
-                color:black;
+                color: white; /* Color de texto blanco */
                 font-weight: bold;
                 font-size: 22px;
                 text-decoration: none;
             }
-            
-            nav a:hover{
-                color: #66ff00;
-                text-decoration: underline;
+
+            /* Estilos para el navbar de Bootstrap */
+            .navbar {
+                background-color: #ff5000; /* Fondo naranja */
+            }
+
+            .navbar .navbar-brand {
+                color: white !important; /* Color de texto blanco importante */
+            }
+
+            .navbar .nav-link {
+                color: white !important; /* Color de texto blanco importante */
             }
         </style>
         <header>
             <img src="../imagenes/logo.png">
-            <nav>
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="productos.html">Productos</a></li>
-                    <li><a href="contacto.html">Contacto</a></li>
-                </ul>
+            <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">Inicio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Rutas y horarios</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Servicios
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Mapa</a></li>
+                      <li><a class="dropdown-item" href="#">Precios</a></li>
+                      <li><a class="dropdown-item" href="#">Unidades</a></li>
+                    </ul>
+                  </li>
+
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Sobre nosotros</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </nav>
         </header>
         `;
