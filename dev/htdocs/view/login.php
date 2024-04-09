34144f4cc8858364ed74bf57f1607b523f8b9a4f
Santiago Solano <sansol2506@gmail.com>
2024-04-08 23:17:00 -0600

    Commit restructuring

--- dev/htdocs/view/login.php
+++ dev/htdocs/view/login.php
@@ -7,8 +7,6 @@
     <link rel="stylesheet" href="../reset.css">
     <link rel="stylesheet" href="login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <script src="../components/header/header.js"></script>
-    <script src="../components/footer/footer.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
--- dev/htdocs/view/servicespage.php
+++ dev/htdocs/view/servicespage.php
@@ -6,7 +6,7 @@
       <title>LA CAZADORA R.L.</title>
       <link rel="stylesheet" href="../css/servicespage.css">
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-      <script src="../bootstrap.min.css"></script>
+      <script src="../css/bootstrap.min.css"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
       <link rel="stylesheet" href="../reset.css">
