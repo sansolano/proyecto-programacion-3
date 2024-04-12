f39674af363e10b1f0b910c0203df3817c191b9d
gabosolarcia <gabosolarcia@gmail.com>
2024-04-11 18:14:47 -0600

    adding 404 error page

---
+++ dev/htdocs/view/errorPage404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Page Not Found</title>
+    <link rel="stylesheet" href="../css/errorPage404.css">
+</head>
+<body>
+    <div class="container">
+         <h2 id="oops" inert>UPS!           
+         </h2>
+         <h4 id="error" inert>Error 404: La pagina que buscaste no existe</h4>   
+    </div>
+</body>
+<script>
+    document.addEventListener("contextmenu", (event) => {
+       event.preventDefault();
+    });
+</script>    
+</html>
